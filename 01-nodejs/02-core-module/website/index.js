a271e9339fa0ba1a5bd055c2961be138591f3009
zafarudin481 <zafarudin.jaffir@gmail.com>
2025-01-22 16:34:58 +0800

    adding comments in the file

--- 01-nodejs/02-core-module/website/index.js
+++ 01-nodejs/02-core-module/website/index.js
@@ -1,13 +1,18 @@
+// importing require nodejs core module
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
+// declaring the port no for nodejs server
 const portNo = 8080;
 
+// creating server
 const server = http.createServer((req, res) => {
+    // extracting the url from the client request
     const url = req.url.toLowerCase();
     console.log(url);
 
+    // using if statement by using extracted url as argument to have different server respond
     if (url === "/") {
         const homePath = path.join(__dirname, "pages", "index.html");
         fs.readFile(homePath, "utf-8", (err, data) => {
@@ -38,12 +43,13 @@ const server = http.createServer((req, res) => {
         return;
     };
 
+    // if none of the above if statements is true, following line would run as default
     res.writeHead(404, { "Content-Type": "text/html" });
     res.write('<h1>404 Not Found</h1>');
     res.end();
 });
 
+// to start  the server by accepting new connections
 server.listen(portNo, () => {
     console.log(`Server is running on port ${portNo}`);
 });
-
