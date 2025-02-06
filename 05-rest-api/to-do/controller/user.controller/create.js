89a7dc4599e472898aba68c0b8d5bdc6286057ae
zafarudin481 <zafarudin.jaffir@gmail.com>
2025-02-06 10:04:13 +0800

    redo the check if email and username is already exist with a single database request

--- 05-rest-api/to-do/controller/user.controller/create.js
+++ 05-rest-api/to-do/controller/user.controller/create.js
@@ -3,16 +3,11 @@ import bcrypt from "bcrypt";
 
 // SQL query
 // $ is placeholder for dynamic value
-const queryCreateUser = `
-INSERT INTO users (username, password, email, is_admin)
-VALUES ($1, $2, $3, $4);
-`;
-const queryCheckEmail = `
-SELECT * FROM users WHERE email = $1;
-`;
-const queryCheckUsername = `
-SELECT * FROM users WHERE username = $1;
-`;
+const queryCreateUser = `INSERT INTO users (username, password, email, is_admin) VALUES ($1, $2, $3, $4);`;
+// // const queryCheckEmail = `SELECT * FROM users WHERE email = $1;`;
+const queryCheckUsername = `SELECT * FROM users WHERE username = $1;`;
+const queryCheckUser = ` SELECT SUM(CASE WHEN email = $1 THEN 1 ELSE 0 END) email_exist, SUM(CASE WHEN username = $2 THEN 1 ELSE 0 END) username_exist
+FROM users;`;
 
 async function createUser(req, res) {
     try {
@@ -40,23 +35,43 @@ async function createUser(req, res) {
             });
         };
 
-        // check if the email and username already exist
-        const dbResCheckEmail = await pool.query(queryCheckEmail, [email]);
-        const dbResCheckUsername = await pool.query(queryCheckUsername, [username]);
-        const data = [dbResCheckEmail.rows, dbResCheckUsername.rows];
-        if (data[0].length != 0 && data[1].length != 0) {
+        // alternative query which only one pool query is used to check if email or username already exist or not
+        const dbResCheckUser = await pool.query(queryCheckUser, [email, username]);
+        const data = dbResCheckUser.rows[0];
+        console.log("Data extract:", data);
+        console.log("email_exist:", data.email_exist);
+        console.log("username_exist:", data.username_exist);
+        if (data.email_exist != 0 && data.username_exist != 0) {
             return res.status(409).json({
                 message: "Email address and username already being used. Please log in or change your email address and username"
             });
-        } else if (data[0].length != 0 && data[1].length == 0) {
+        } else if (data.email_exist != 0 && data.username_exist == 0) {
             return res.status(409).json({
-                message: "An account with this email address already exists. Please log in or use a different email to register."
+                message: "An account with this email address already exists. Please log in or use a different email to register"
             });
-        } else if (data[0].length == 0 && data[1].length != 0) {
+        } else if (data.email_exist == 0 && data.username_exist != 0) {
             return res.status(409).json({
-                message: "Username is already being used. Please use different username."
+                message: "Username is already being used. Please use different username"
             });
         };
+
+        // check if the email and username already exist
+        // const dbResCheckEmail = await pool.query(queryCheckEmail, [email]);
+        // const dbResCheckUsername = await pool.query(queryCheckUsername, [username]);
+        // const data = [dbResCheckEmail.rows, dbResCheckUsername.rows];
+        // if (data[0].length != 0 && data[1].length != 0) {
+        //     return res.status(409).json({
+        //         message: "Email address and username already being used. Please log in or change your email address and username"
+        //     });
+        // } else if (data[0].length != 0 && data[1].length == 0) {
+        //     return res.status(409).json({
+        //         message: "An account with this email address already exists. Please log in or use a different email to register."
+        //     });
+        // } else if (data[0].length == 0 && data[1].length != 0) {
+        //     return res.status(409).json({
+        //         message: "Username is already being used. Please use different username."
+        //     });
+        // };
 
         // hash password
         const salt = bcrypt.genSaltSync(SALTROUNDS);
@@ -64,13 +79,12 @@ async function createUser(req, res) {
 
         // create user in database using query
         const dbRes = await pool.query(queryCreateUser, [username, hashPassword, email, isAdmin]);
-        console.log(dbRes);
         res.status(201).json({
             message: "User is created"
         });
     } catch (error) {
         // rewrite error message because it is not a good practice to expose database error message directly to the client
-        console.error(error);
+        //console.error(error);
         res.status(500).json({
             message: "Internal server error"
         });
--- 05-rest-api/to-do/controller/user.controller/update.js
+++ 05-rest-api/to-do/controller/user.controller/update.js
@@ -4,6 +4,8 @@ import { pool } from "../../database/connection.js"
 const queryUpdateUserById = `UPDATE users SET username = $1, email = $2 WHERE id = $3`;
 const queryCheckEmail = `SELECT * FROM users WHERE email = $1 AND id != $2;`;
 const queryCheckUsername = `SELECT * FROM users WHERE username = $1 AND id != $2;`;
+const queryCheckUser = `SELECT SUM(CASE WHEN email = $1 THEN 1 ELSE 0 END) email_exist, SUM(CASE WHEN username = $2 THEN 1 ELSE 0 END) username_exist
+FROM users WHERE id != $3;`;
 
 
 async function updateUser(req, res) {
@@ -28,24 +30,40 @@ async function updateUser(req, res) {
             });
         };
 
-        // check if new email and username is already exist
-        const dbResCheckEmail = await pool.query(queryCheckEmail, [email, id]);
-        const dbResCheckUsername = await pool.query(queryCheckUsername, [username, id]);
-        const data = [dbResCheckEmail.rows, dbResCheckUsername.rows];
-        if (data[0].length != 0 && data[1].length != 0) {
+        // alternative query to check email and username is already exist or not
+        const dbResCheckUser = await pool.query(queryCheckUser, [email, username, id]);
+        const data = dbResCheckUser.rows[0];
+        if (data.email_exist != 0 && data.username_exist != 0) {
             return res.status(409).json({
-                message: "Email address and username already being used. Please change your new email address and username"
+                message: "Email address and username already being used. Please change to different email and username"
             });
-        } else if (data[0].length != 0 && data[1].length == 0) {
+        } else if (data.email_exist != 0 && data.username_exist == 0) {
             return res.status(409).json({
-                message: "An account with this email address already exists. Please use different email"
+                message: "An account with this email address already exists. Please use different email address"
             });
-        } else if (data[0].length == 0 && data[1].length != 0) {
+        } else if (data.email_exist == 0 && data.username_exist != 0) {
             return res.status(409).json({
-                message: "Username is already being used. Please use different username."
+                message: "Username is already being used. Please use different username"
             });
         };
 
+        // check if new email and username is already exist
+        // const dbResCheckEmail = await pool.query(queryCheckEmail, [email, id]);
+        // const dbResCheckUsername = await pool.query(queryCheckUsername, [username, id]);
+        // const data = [dbResCheckEmail.rows, dbResCheckUsername.rows];
+        // if (data[0].length != 0 && data[1].length != 0) {
+        //     return res.status(409).json({
+        //         message: "Email address and username already being used. Please change your new email address and username"
+        //     });
+        // } else if (data[0].length != 0 && data[1].length == 0) {
+        //     return res.status(409).json({
+        //         message: "An account with this email address already exists. Please use different email"
+        //     });
+        // } else if (data[0].length == 0 && data[1].length != 0) {
+        //     return res.status(409).json({
+        //         message: "Username is already being used. Please use different username."
+        //     });
+        // };
 
         await pool.query(queryUpdateUserById, [username, email, id]);
         res.status(200).json({
